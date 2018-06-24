7674c55d14bc3c3765973b4bc6ddda67f799e6d8
Janaka Perera <it15110008@my.sliit.lk>
2018-06-24 13:02:04 +0530

    added new functions and modifiers

--- contracts/IotSecurity.sol
+++ contracts/IotSecurity.sol
@@ -2,28 +2,27 @@ pragma solidity ^0.4.24;
 
 contract IotSecurity {
     
-    address [] public user_arr;
-    uint public user_arr_length;
+    address[] public user_arr;
     string public user_permission ="Not Available";
-    
+
     struct deviceUserInfo {
         address user;
         string data;
         string permission;
     }
-    
+   
     struct user_info {
         address device;
         string permission;
     }
     
     struct user {
-        user_info [] users_devices;
+        user_info[] users_devices;
     }
     
     struct device {
         address owner;
-        deviceUserInfo [] DeviceInfo;
+        deviceUserInfo[] DeviceInfo;
     }
     
     event e_Permission (string permission);
@@ -32,26 +31,43 @@ contract IotSecurity {
     
     mapping(address => user) users;
     
-    function addUsersToDevice (string permission, address deviceAddress, address userAddress) public {
+    //check the owner of the device - there can be only one owner per device
+    modifier owner(address checkDevice){
+        address checkOwner;
+        checkOwner = devices[checkDevice].owner;
+        require(msg.sender == checkOwner);
+        _;
+    }
+    
+    //check if the device has an owner or a new device ( new device has the address of 0x00000000000000000000)
+    modifier firstUser(address dAddr){
+        require(devices[dAddr].owner == 0x00000000000000000000);
+        _;
+    }
+    
+    owner can add users to his owned devices
+    function addUsersToDevice (string permission, address deviceAddress, address userAddress) public owner(deviceAddress){
         deviceUserInfo memory obj = deviceUserInfo({
             user: userAddress,
             data: "null",
             permission: permission
         });
-
         devices[deviceAddress].DeviceInfo.push(obj);
     }
+    
 
-    function getUsersFromDevice (address devAddress) public {
+    //owner can check all the users added to the device he owns
+    function getUsersFromDevice (address devAddress) public owner(devAddress) returns (address[]){
         delete user_arr;
-        for (uint i =0 ; i < devices[devAddress].DeviceInfo.length ; i++){
+        for (uint i = 0 ; i < devices[devAddress].DeviceInfo.length ; i++){
             user_arr.push(devices[devAddress].DeviceInfo[i].user);
         }
-        // return user_arr;
+        return user_arr;
     }
     
+    // //owner can check permission for a given user and device
     function getUserPermissionsForaDevice (address devAddress, address userAddress) public returns (string){
-        for (uint i =0 ; i < devices[devAddress].DeviceInfo.length ; i++){
+        for (uint i = 0 ; i < devices[devAddress].DeviceInfo.length ; i++){
             if (devices[devAddress].DeviceInfo[i].user == userAddress){
                 user_permission = devices[devAddress].DeviceInfo[i].permission;
                 break;
@@ -59,9 +75,10 @@ contract IotSecurity {
         }
         return user_permission;
     }
+   
     
     function changePermission (address devAddress, address userAddress, string permission) public {
-        for (uint i =0 ; i < devices[devAddress].DeviceInfo.length ; i++){
+        for (uint i = 0 ; i < devices[devAddress].DeviceInfo.length ; i++){
             if (devices[devAddress].DeviceInfo[i].user == userAddress){
                 devices[devAddress].DeviceInfo[i].permission = permission;
                 emit e_Permission(permission);
@@ -70,20 +87,38 @@ contract IotSecurity {
         }
     }
     
-    function addDeviceToUser (address devAddress) public {
-        user_info memory obj = user_info({
-            device: devAddress,
-            permission: "Read/Write"
-        });
-        users[msg.sender].users_devices.push(obj);
-        
-        // getUsersDevices()
-
+    
+    //check all the available devices for a given account
+    function getUsersDevices() public returns (address[]){
         delete user_arr;
         for (uint i = 0; i < users[msg.sender].users_devices.length; i++){
             user_arr.push(users[msg.sender].users_devices[i].device);
         }
-        user_arr_length = user_arr.length;
+        return user_arr;
+    }
+    
+
+    //add the owner of the device 
+    function addOwner(address newDevice) public firstUser(newDevice){
+        devices[newDevice].owner = msg.sender;
+        deviceUserInfo memory obj = deviceUserInfo({
+            user: msg.sender,
+            data: "null",
+            permission: "Read/Write"
+        });
+        devices[newDevice].DeviceInfo.push(obj);
+
+        user_info memory obj2 = user_info({
+            device: newDevice,
+            permission: "Read/Write"
+        });
+
+        users[msg.sender].users_devices.push(obj2);
+    }
+    
+    //view the owner of the device
+    function viewOwner(address deviceAddr) public view returns (address){
+        return (devices[deviceAddr].owner);
     }
     
 }
